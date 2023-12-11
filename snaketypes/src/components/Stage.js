e4dd91060f519e5261d26d455e7bad43e4845296
ZahraaEzzat <s2201131107@sci.ku.edu.kw>
2023-12-12 01:14:57 +0300

    x

--- snaketypes/src/components/Stage.js
+++ snaketypes/src/components/Stage.js
@@ -1,182 +1,130 @@
-import { useSelector } from "react-redux";
+// import {Link} from 'react-router-dom'
+import Side from "./Side";
+// import App from './App';
+import { Provider } from "react-redux";
+import store from "../store/actions/actions.ts";
+import { ChakraProvider, Container, Heading } from "@chakra-ui/react";
 import CanvasBoard from "./snake/CanvasBoard.tsx";
-import '../assets/css/Stage.css';
-import { useEffect, useState, useCallback } from 'react';
-import ScoreCard from './snake/ScoreCard.tsx';
-import { useDispatch } from 'react-redux';
-import { fetchObj, getRandomWords, correctColoring, spanWord } from "../assets/js/utils.js";
-import { makeMove, MOVE_RIGHT, MOVE_LEFT, MOVE_UP, MOVE_DOWN, increaseSnake, INCREMENT_SCORE, scoreUpdates, stopGame, RESET_SCORE, resetGame, setWords } from "../store/actions/actions.ts";
-
-
+import "../assets/css/Stage.css";
+import { useEffect, useState } from "react";
+import MiniProfile from "./MiniProfile.js";
 
 const Stage = () => {
-    const h = 570;
-    const w = 1000;
-    const dispatch = useDispatch();
-    const [wordsDirection, setWordsDirection] = useState([])
-    const disallowedDirection = useSelector((state: IGlobalState) => state.disallowedDirection);
+  const h = 570;
+  const w = 1000;
 
-    const upWord = useSelector((state: IGlobalState) => state.upWord);
-    const downWord = useSelector((state: IGlobalState) => state.downWord);
-    const rightWord = useSelector((state: IGlobalState) => state.rightWord);
-    const leftWord = useSelector((state: IGlobalState) => state.leftWord);
+  // later will be pulled from database
+  const words = [
+    "apple",
+    "peach",
+    "cherry",
+    "grape",
+    "coconut",
+    "mango",
+    "orange",
+    "avocado",
+    "pomegranate",
+    "strawberry",
+  ];
+  // const words = ['orange']
 
-    // later will be pulled from database
-    const words = ['apple', 'peach', 'cherry', 'grape', 'coconut', 'mango', 'orange', 'avocado', 'pomegranate', 'strawberry'];
-    // const words = ['orange']
+  const [word, setWord] = useState("word"); // the word that should be typed
+  const [trackIdx, setTrackIdx] = useState(0); // keeps track of the last correct character the user typed
 
-    const [word, setWord] = useState('word');       // the word that should be typed
-    const [trackIdx, setTrackIdx] = useState(0);    // keeps track of the last correct character the user typed
+  // returns a random element from a list, this will be removed as later on we can request a random word from database
+  function get_random(list) {
+    let idx = Math.floor(Math.random() * list.length);
+    return list[idx];
+  }
 
-    const [upTrackIdx, setUpTrackIdx] = useState(0);    // keeps track of the last correct character the user typed
-    const [downTrackIdx, setDownTrackIdx] = useState(0);    // keeps track of the last correct character the user typed
-    const [rightTrackIdx, setRightTrackIdx] = useState(0);    // keeps track of the last correct character the user typed
-    const [leftTrackIdx, setLeftTrackIdx] = useState(0);    // keeps track of the last correct character the user typed
+  // the first time the page loads, pick a word and fill wordDiv with characters
+  useEffect(() => {
+    let sync_word = get_random(words);
+    console.log("word: ", sync_word);
 
-    const moveSnake = useCallback(
-        (dx = 0, dy = 0, ds = '') => {
-          if (dx > 0 && dy === 0 && ds !== "RIGHT") {
-            // console.log('dispatching: ', makeMove(dx, dy, MOVE_RIGHT))
-            dispatch(makeMove(dx, dy, MOVE_RIGHT));
-          }
-    
-          if (dx < 0 && dy === 0 && ds !== "LEFT") {
-            // console.log('dispatching: ', makeMove(dx, dy, MOVE_LEFT))
-            dispatch(makeMove(dx, dy, MOVE_LEFT));
-          }
-    
-          if (dx === 0 && dy < 0 && ds !== "UP") {
-            // console.log('dispatching: ', makeMove(dx, dy, MOVE_UP))
-            dispatch(makeMove(dx, dy, MOVE_UP));
-          }
-    
-          if (dx === 0 && dy > 0 && ds !== "DOWN") {
-            // console.log('dispatching: ', makeMove(dx, dy, MOVE_DOWN))
-            dispatch(makeMove(dx, dy, MOVE_DOWN));
-          }
-        },
-        [dispatch]
-      );
-
-    const wordsInit = useCallback(async ()=>{
-        
-        let wordsArray_fetched = await fetchObj('words');
-        let wordsArray_filtered = getRandomWords(wordsArray_fetched, 4);
-        dispatch(setWords(wordsArray_filtered));
-
-        // setting the word to be typed
-        let sync_word = getRandomWords(wordsArray_filtered, 1)[0].wordText;
-        setWord(sync_word);
-
-        setWordsDirection([
-            {text: wordsArray_filtered[0], direction: 'up'},
-            {text: wordsArray_filtered[1], direction: 'down'},
-            {text: wordsArray_filtered[2], direction: 'right'},
-            {text: wordsArray_filtered[3], direction: 'left'},
-        ])
-
-        // console.log('filtered: ', setWords(wordsArray_filtered))
-      }, [dispatch])
-
-    // the first time the page loads, pick a word and fill wordDiv with characters
-    useEffect(()=>{
-        wordsInit();
-    }, [] )
-
-    useEffect(()=>{
-         // setting wordDiv
-         let wordDivs = document.getElementsByClassName('word');
-        //  wordDivs[0].innerHTML = spanWord(upWord);
-        //  wordDivs[1].innerHTML = spanWord(downWord);
-        //  wordDivs[2].innerHTML = spanWord(rightWord);
-        //  wordDivs[3].innerHTML = spanWord(leftWord);
-         wordDivs[0].innerHTML = spanWord(upWord.wordText);
-         wordDivs[1].innerHTML = spanWord(downWord.wordText);
-         wordDivs[2].innerHTML = spanWord(rightWord.wordText);
-         wordDivs[3].innerHTML = spanWord(leftWord.wordText);
-    }, [upWord])
-
-   
-
-    // called whenever the user inputs a character or removes one
-    function trackWord(){
-        let currentValue = document.getElementsByClassName('user-input')[0];        // user input
-        let wordDiv_arr = document.getElementsByClassName('word')[0].childNodes;    // wordDiv
-        let lastPressed = currentValue.value[currentValue.value.length - 1];        // last character the user typed
-
-        // filter excess elements, childNodes returns unnecessary elements so they have to be removed
-        let temp = [];
-        for(let c = 0; c < word.length; c++){
-            temp.push(wordDiv_arr[c*2])
-        }
-        wordDiv_arr = temp;
-
-        // handle when <backspace> is used
-        if(currentValue.value.length + 1 == trackIdx){
-            correctColoring(wordDiv_arr, trackIdx - 1);
-            setTrackIdx(trackIdx - 1);
-            return;
-        }
-
-        // if correct character was typed increment the index of the (last correct character)
-        /*
-         * 
-         */
-        if(lastPressed == word[trackIdx]){
-            if(trackIdx + 1 == wordDiv_arr.length){
-                console.log('word completed: ', word)
-
-                switch(word){
-                    case upWord.wordText:
-                        moveSnake(0, -20, disallowedDirection);
-                        break;
-                    case downWord.wordText:
-                        moveSnake(0, 20, disallowedDirection);
-                        break;
-                    case rightWord.wordText:
-                        moveSnake(20, 0, disallowedDirection);
-                        break;
-                    case leftWord.wordText:
-                        moveSnake(-20, 0, disallowedDirection);
-                        break;
-
-                    default:
-                        console.log('word completed default case');
-                }
-
-            }
-            setTrackIdx(trackIdx + 1);
-            correctColoring(wordDiv_arr, trackIdx + 1);
-        }
-        else{
-            currentValue.value = currentValue.value.slice(0, -1);   // remove the incorrect character
-        }
-
+    setWord(sync_word);
+    let wordDiv = document.getElementsByClassName("word")[0];
+    let html = "";
+    for (let c = 0; c < sync_word.length; c++) {
+      html += `<span class="word-char" style="font-size: xxx-large; color: gray; letter-spacing: 3px;">${sync_word[c]}</span>\n`;
     }
 
-    return ( 
+    wordDiv.innerHTML = html;
+  }, []);
 
-                <div className='Stage'>
-                    <ScoreCard />
+  // colors the wordDiv until the last correct character (this keeps track of what is being typed)
+  function correctColoring(wordDiv_arr, tIdx) {
+    let charElement = null;
 
-                    <div className="wordDivs">
-                        <div className='word' id="upWord"></div>
-                        <div className='word' id="downWord"></div>
-                        <div className='word' id="rightWord"></div>
-                        <div className='word' id="leftWord"></div>
-                    </div>
-                    <input
-                        className='user-input'
-                        type='text'
-                        onChange={trackWord}
-                        onBlur={()=>{document.getElementsByClassName('user-input')[0].focus()}} 
-                        autoFocus
-                    />
-                    <CanvasBoard width={w} height={h} />
+    // black color up to tIdx (the last correct character)
+    for (let c = 0; c < tIdx; c++) {
+      charElement = wordDiv_arr[c];
+      charElement.style.color = "black";
+    }
 
-                </div>
-        
-     );
-}
- 
+    // the rest should remain gray (we are using a loop to avoid black-gray-black patterns)
+    for (let c = tIdx; c < wordDiv_arr.length; c++) {
+      charElement = wordDiv_arr[c];
+      charElement.style.color = "gray";
+    }
+  } // end correctColoring
+
+  // called whenever the user inputs a character or removes one
+  function trackWord() {
+    let currentValue = document.getElementsByClassName("user-input")[0]; // user input
+    let wordDiv_arr = document.getElementsByClassName("word")[0].childNodes; // wordDiv
+    let lastPressed = currentValue.value[currentValue.value.length - 1]; // last character the user typed
+
+    // filter excess elements, childNodes returns unnecessary elements so they have to be removed
+    let temp = [];
+    for (let c = 0; c < word.length; c++) {
+      temp.push(wordDiv_arr[c * 2]);
+    }
+    wordDiv_arr = temp;
+
+    // console.log('current value: ', currentValue.value, ' last pressed: ', lastPressed, '\nidx: ', trackIdx, '\nleng: ', currentValue.value.length);
+    // handle when <backspace> is used
+    if (currentValue.value.length + 1 == trackIdx) {
+      console.log("backspace caught");
+      correctColoring(wordDiv_arr, trackIdx - 1);
+      setTrackIdx(trackIdx - 1);
+      return;
+    }
+
+    // if correct character was typed increment the index of the (last correct character)
+    if (lastPressed == word[trackIdx]) {
+      if (trackIdx + 1 == wordDiv_arr.length) {
+        console.log("word completed");
+        console.log('Dispatching: "MOVE_UP"');
+      }
+      setTrackIdx(trackIdx + 1);
+      correctColoring(wordDiv_arr, trackIdx + 1);
+    } else {
+      currentValue.value = currentValue.value.slice(0, -1); // remove the incorrect character
+    }
+  }
+
+  return (
+    <div className="Stage">
+      <MiniProfile />
+      <div className="word"></div>{" "}
+      {/* Gets filled with span elements for each character */}
+      <input
+        className="user-input"
+        type="text"
+        onChange={trackWord}
+        onBlur={() => {
+          document.getElementsByClassName("user-input")[0].focus();
+        }}
+        autoFocus
+      />
+      <Provider store={store}>
+        {/* <ChakraProvider> */}
+        <CanvasBoard width={w} height={h} />
+        {/* </ChakraProvider> */}
+      </Provider>
+    </div>
+  );
+};
+
 export default Stage;
